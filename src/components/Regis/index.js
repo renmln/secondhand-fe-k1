d3ad17f507cf2603e8f99ec5041abd34ac63f038
Karolina Eka Purnama <ekapurnamakarolina@gmail.com>
2022-06-15 00:22:03 +0300

    login regis

---
+++ src/components/Regis/index.js
@@ -0,0 +1,47 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../../images/img.png';
+import '../../css/style.css';
+import icon from '../../images/img.png';
+
+export default function Regis() {
+    return (
+<section class="Form my-4 mx-5">
+        <div class="container">
+            <div class="row no-gutters">
+                <div class="col-lg-5">
+                <img class="img-fluid" src={icon} alt="" />
+                </div>
+                <div class="col-lg-7 px-5 pt-5">
+                    <h1 class="font-weight-bold py-3">Daftar</h1>
+                    <form>
+                        <div class="form-row">
+                            <div class="col-lg-7">
+                                <input type="nama" placeholder="Nama Lengkap" class="form-control my-3 p-4"/>
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="col-lg-7">
+                                <input type="email" placeholder="johndee@gmail.com" class="form-control my-3 p-4"/>
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="col-lg-7">
+                            <input type="password" placeholder="Masukkan Password" class="form-control my-3 p-4"/>
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="col-lg-7">
+                        <button type="button" class="btn1 mt-3 mb-5">Daftar</button>
+                    </div>
+                </div>
+                <p>Sudah punya akun? <a href="/login">Masuk disini</a></p>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+    )
+    
+}
+
--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import { Login } from './components';
+import { Regis } from './components';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login/>} />
+        <Route path="/regis" element={<Regis/>} />
+      </Routes>
+    </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
