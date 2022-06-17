8a2f718b6a2cbf96490b2529f134fdf44bde6529
Wrendra57 <wrendra57@gmail.com>
2022-06-17 18:01:13 +0700

    login regis sementara

--- src/component/Login/index.js
+++ src/component/Login/index.js
@@ -1,41 +1,109 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../images/img.png';
 import '../../css/style.css';
 import icon from '../../images/img.png';
+import { Navigate } from "react-router-dom";
+
+async function doLogin({ email, password }) {
+    // Gunakan endpoint-mu sendiri
+    const response = await fetch("http://localhost:8000/api/v1/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            email,
+            password,
+        }),
+    });
+    const data = await response.json();
+    return data.token;
+}
+
+
+
+
 
 export default function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+    const token = localStorage.getItem("token");
+
+    useEffect(() => {
+        setIsLoggedIn(!!token);
+    }, [token]);
+
+    function handleSubmit(e) {
+        setIsLoading(true);
+        e.preventDefault();
+        doLogin({ email, password })
+            .then((token) => localStorage.setItem("token", token))
+            .catch((err) => console.log(err.message))
+            .finally(() => setIsLoading(false));
+    }
+
+    function handleLogout(e) {
+        setIsLoading(true);
+        e.preventDefault();
+        localStorage.removeItem("token");
+        setIsLoggedIn(false);
+        setIsLoading(false);
+    }
     return (
-        <section class="Form my-4 mx-5">
-        <div class="container">
-            <div class="row no-gutters">
-                <div class="col-lg-5">
-                <img class="img-fluid img1" src={icon} alt="" />
-                </div>
-                <div class="col-lg-7 px-5 pt-5">
-                    <h1 class="font-weight-bold py-3">Masuk</h1>
-                    <form>
-                        <div class="form-row">
-                            <div class="col-lg-7">
-                                <input type="email" placeholder="johndee@gmail.com" class="form-control my-3 p-4"/>
-                            </div>
-                        </div>
-                        <div class="form-row">
-                            <div class="col-lg-7">
-                            <input type="password" placeholder="Masukkan Password" class="form-control my-3 p-4"/>
-                        </div>
+        <section className="Form my-4 mx-5">
+            <div className="container">
+                <div className="row no-gutters">
+                    <div className="col-lg-5">
+                        <img className="img-fluid img1" src={icon} alt="" />
                     </div>
-                    <div class="form-row">
-                        <div class="col-lg-7">
-                        <button type="button" class="btn1 mt-3 mb-5">Masuk</button>
+                    <div className="col-lg-7 px-5 pt-5">
+                        <h1 className="font-weight-bold py-3">Masuk</h1>
+                        {!isLoggedIn ? (
+                            <form onSubmit={handleSubmit}>
+                                <div className="form-row">
+                                    <div className="col-lg-7">
+                                        <input
+                                            type="email"
+                                            placeholder="johndee@gmail.com"
+                                            onChange={(e) => setEmail(e.target.value)}
+                                            className="form-control my-3 p-4"
+                                            value={email}
+
+                                        />
+                                    </div>
+                                </div>
+                                <div className="form-row">
+                                    <div className="col-lg-7">
+                                        <input
+                                            type="password"
+                                            placeholder="Masukkan Password"
+                                            className="form-control my-3 p-4"
+                                            onChange={(e) => setPassword(e.target.value)}
+                                            value={password}
+                                        />
+                                    </div>
+                                </div>
+                                <div className="form-row">
+                                    <div className="col-lg-7">
+                                        <input type="submit"
+                                            value={isLoading ? "Loading" : "Login"}
+                                            className="btn1 mt-3 mb-5">
+                                        </input>
+                                    </div>
+                                </div>
+                                <p>Belum punya akun? <a href="/regis">Daftar disini</a></p>
+                            </form>
+                        ) : (
+                            <Navigate to="/halamanproduk" />
+                        )}
+
                     </div>
-                </div>
-                <p>Belum punya akun? <a href="/regis">Daftar disini</a></p>
-                    </form>
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
     )
 }
-    
+
--- src/component/Regis/index.js
+++ src/component/Regis/index.js
@@ -1,38 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../images/img.png';
 import '../../css/style.css';
 import icon from '../../images/img.png';
+import axios from "axios";
+import { useNavigate } from 'react-router-dom'
 
 export default function Regis() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    // const [msg, setMsg] = useState('');
+    const navigate = useNavigate();
+
+    const Register = async (e) => {
+        // biar onosubmit tidak terefresh
+        e.preventDefault();
+        try {
+            await axios.post('http://localhost:8000/api/v1/register',
+                {
+                    email: email,
+                    password: password
+                });
+            // redirect
+
+            navigate('/')
+        } catch (error) {
+            if (error.respone) {
+                console.log(error.respone.data)
+            }
+
+        }
+    }
+
     return (
-        <section class="Form my-4 mx-5">
-            <div class="container">
-                <div class="row no-gutters">
-                    <div class="col-lg-5">
-                        <img class="img-fluid img1" src={icon} alt="" />
+        <section className="Form my-4 mx-5">
+            <div className="container">
+                <div className="row no-gutters">
+                    <div className="col-lg-5">
+                        <img className="img-fluid img1" src={icon} alt="" />
                     </div>
-                    <div class="col-lg-7 px-5 pt-5">
-                        <h1 class="font-weight-bold py-3">Daftar</h1>
-                        <form>
-                            <div class="form-row">
-                                <div class="col-lg-7">
-                                    <input type="nama" placeholder="Nama Lengkap" class="form-control my-3 p-4" />
+                    <div className="col-lg-7 px-5 pt-5">
+                        <h1 className="font-weight-bold py-3">Daftar</h1>
+                        <form onSubmit={Register}>
+                            <div className="form-row">
+                                <div className="col-lg-7">
+                                    <input type="nama" placeholder="Nama Lengkap" className="form-control my-3 p-4" />
                                 </div>
                             </div>
-                            <div class="form-row">
-                                <div class="col-lg-7">
-                                    <input type="email" placeholder="johndee@gmail.com" class="form-control my-3 p-4" />
+                            <div className="form-row">
+                                <div className="col-lg-7">
+                                    <input
+                                        type="email"
+                                        placeholder="johndee@gmail.com"
+                                        className="form-control my-3 p-4"
+                                        value={email}
+                                        onChange={(e) => setEmail(e.target.value)}
+                                    />
                                 </div>
                             </div>
-                            <div class="form-row">
-                                <div class="col-lg-7">
-                                    <input type="password" placeholder="Masukkan Password" class="form-control my-3 p-4" />
+                            <div className="form-row">
+                                <div className="col-lg-7">
+                                    <input
+                                        type="password"
+                                        placeholder="Masukkan Password"
+                                        className="form-control my-3 p-4"
+                                        value={password}
+                                        onChange={(e) => setPassword(e.target.value)} />
+
                                 </div>
                             </div>
-                            <div class="form-row">
-                                <div class="col-lg-7">
-                                    <button type="button" class="btn1 mt-3 mb-5">Daftar</button>
+                            <div className="form-row">
+                                <div className="col-lg-7">
+                                    {/* <input type="submit" className="btn1 mt-3 mb-5" value={"Daftar"} /> */}
+                                    <button className="button is-success is-fullwidth">Daftar</button>
                                 </div>
                             </div>
                             <p>Sudah punya akun? <a href="/login">Masuk disini</a></p>
