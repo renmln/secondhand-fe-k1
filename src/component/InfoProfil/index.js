c31dbaa38a8193824c7e30503ea171b8f57de78e
Wrendra57 <wrendra57@gmail.com>
2022-06-22 17:05:07 +0700

    editprofil sementara

--- src/component/InfoProfil/index.js
+++ src/component/InfoProfil/index.js
@@ -1,10 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import arrow from "../../images/fi_arrow-left.png";
 import Rectangle127 from "../../images/Rectangle127.svg";
 import Group1 from "../../images/Group_1.png"
+import { useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
 
 export default function InfoProfil() {
+  const [name, setName] = useState("");
+  const [photo_profile, setPhoto_profile] = useState("");
+  const [city, setCity] = useState("");
+  const [address, setAddress] = useState("");
+  const [no_hp, setNo_hp] = useState("");
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  useEffect(() => {
+    getUserById();
+  }, []);
+
+  const updateUser = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.put(`http://localhost:8000/api/v1/profile/update/${id}`, {
+        name: name,
+        photo_profile: photo_profile,
+        city: city,
+        address: address,
+        no_hp: no_hp
+      });
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getUserById = async () => {
+    const response = await axios.get(`http://localhost:8000/api/v1/users/${id}`);
+    console.log(response)
+    setName(response.data.name);
+    setPhoto_profile(response.data.photo_profile);
+    setCity(response.data.city);
+    setAddress(response.data.address);
+    setNo_hp(response.data.no_hp);
+  };
+
   return (
     <div>
       <nav className="navbar navbar-expand-lg bg-light d-inline-flex"
@@ -25,36 +65,80 @@ export default function InfoProfil() {
 
 
       <section>
-        <form>
+        <form onSubmit={updateUser}>
           <div className="container" style={{ padding: '30px', width: '70%' }}>
             <a href="/"> <img src={arrow} alt="" /></a>
             <label for="file-input" style={{ justifyContent: 'center', display: 'flex', alignItems: 'center' }}>
               <img src={Group1} alt="" />
             </label>
-            <input id='file-input' type="file" style={{ display: 'none' }} accept=".jpg,.jpeg,.png" />
+            <input
+              id='file-input'
+              type="file"
+              style={{ display: 'none' }}
+              accept=".jpg,.jpeg,.png"
+              value={photo_profile}
+              onChange={(e) => setPhoto_profile(e.target.value)}
+
+            />
             <div className="mb-3">
               <label for="nama" className="form-label">Nama<span style={{ color: 'red}' }}>*</span></label>
-              <input type="text" className="form-control" id="nama" placeholder="Nama" required style={{ borderRadius: '16px' }} />
+              <input
+                type="text"
+                className="form-control"
+                id="nama" placeholder="Nama"
+                required
+                style={{ borderRadius: '16px' }}
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
             </div>
             <div className="mb-3">
               <label for="kota" className="form-label">Kota<span style={{ color: 'red' }}>*</span></label>
-              <select className="form-select" id="kota" required style={{ borderRadius: '16px' }}>
+              <input
+                type="text"
+                className="form-control"
+                id="kota" placeholder="Kota"
+                required
+                style={{ borderRadius: '16px' }}
+                value={city}
+                onChange={(e) => setCity(e.target.value)}
+              />
+              {/* <select className="form-select" id="kota" required style={{ borderRadius: '16px' }}>
                 <option selected>Pilih Kota</option>
                 <option value="1"> Jakarta</option>
                 <option value="2"> Surabaya</option>
                 <option value="3"> Bandung</option>
-              </select>
+              </select> */}
             </div>
             <div className="mb-3">
               <label for="alamat" className="form-label">Alamat<span style={{ color: 'red' }}>*</span></label>
-              <input type="text" className="form-control" id="alamat" placeholder="contoh: Jalan Ikan Hiu 33" required style={{ borderRadius: '16px' }} />
+              <input
+                type="text"
+                className="form-control"
+                id="alamat"
+                placeholder="contoh: Jalan Ikan Hiu 33"
+                required
+                style={{ borderRadius: '16px' }}
+                value={address}
+                onChange={(e) => setAddress(e.target.value)} />
             </div>
             <div className="mb-3">
               <label for="nohp" className="form-label">No. Handphone<span style={{ color: 'red' }}>*</span></label>
-              <input type="number" min='11' className="form-control" id="nohp" placeholder="contoh: 08123456789" required style={{ borderRadius: '16px' }} />
+              <input
+                type="number"
+                min='11'
+                className="form-control"
+                id="nohp"
+                placeholder="contoh: 08123456789"
+                required
+                style={{ borderRadius: '16px' }}
+                value={no_hp}
+                onChange={(e) => setNo_hp(e.target.value)} />
             </div>
             <div className="mb-3">
-              <button type="submit" className="btn btn-primary" style={{ width: '100%', backgroundColor: '#7126B5', borderRadius: '16px' }}>Simpan</button>
+              <button type="submit" className="btn btn-primary" style={{ width: '100%', backgroundColor: '#7126B5', borderRadius: '16px' }}>
+                Simpan
+              </button>
             </div>
           </div>
         </form>
--- src/component/NavBar/index.js
+++ src/component/NavBar/index.js
@@ -19,43 +19,12 @@ export default function NavBar() {
         setIsLoggedIn(!!token);
     }, [token]);
 
-    // function handleSubmit(e) {
-    //     setIsLoading(true);
-    //     e.preventDefault();
-    //     doLogin({ email, password })
-    //         .then((token) => localStorage.setItem("token", token))
-    //         .catch((err) => console.log(err.message))
-    //         .finally(() => setIsLoading(false));
-    // }
-
     function handleLogout(e) {
         e.preventDefault();
         localStorage.removeItem("token");
         setIsLoggedIn(false);
     }
     return (
-        // <header>
-        //     <nav className="navbar navbar-expand-lg navbar-light bg-light  container">
-        //         <div className="container-fluid">
-        //             <a className="navbar-brand " href="/"><img src={icon} alt="" /></a>
-        //             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
-        //                 aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        //                 <span className="navbar-toggler-icon"></span>
-        //             </button>
-        //             <form className="d-flex border">
-        //                 <input className="form-control me-2 border-0" type="search" placeholder="Search" aria-label="Search"></input>
-        //                 <button className="btn" type="submit"><FiSearch /></button>
-        //             </form>
-        //             <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        //                 <ul className="navbar-nav ms-auto mb-2 mx-5 mb-lg-0">
-        //                     <li className="nav-item px-1">
-        //                         <a href='/#' type="button" className="btn btn-sm btn-custom nav-link text-light rounded-12px active"> <FiLogIn /> Masuk</a>
-        //                     </li>
-        //                 </ul>
-        //             </div>
-        //         </div>
-        //     </nav>
-        // </header>
 
         <div>
             <Navbar collapseOnSelect expand="lg" bg="light" variant='light'>
@@ -146,7 +115,7 @@ export default function NavBar() {
                                     </NavDropdown.Item>
 
                                 </NavDropdown>
-                                <Nav.Link href="/infoprofil">
+                                <Nav.Link href="/infoprofil/">
                                     <button type="button"
                                         className="btn btn-sm nav-link text-dark rounded-12px active">
                                         <FiUser />
