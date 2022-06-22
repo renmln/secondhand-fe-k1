27ac78c942c1ec97a78fbda92da463155541ef5f
renmln <renamuliani@gmail.com>
2022-06-22 22:00:02 +0700

    add cloudinary

--- src/component/InfoProfil/index.js
+++ src/component/InfoProfil/index.js
@@ -1,41 +1,56 @@
 import React, { useState, useEffect } from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import arrow from "../../images/fi_arrow-left.png";
 import Rectangle127 from "../../images/Rectangle127.svg";
-import Group1 from "../../images/Group_1.png"
+import Group1 from "../../images/Group_1.png";
 import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
 
 export default function InfoProfil() {
   const [name, setName] = useState("");
-  const [photo_profile, setPhoto_profile] = useState("");
+  const [photo_profile, setPhotoProfile] = useState("");
   const [city, setCity] = useState("");
   const [address, setAddress] = useState("");
   const [no_hp, setNo_hp] = useState("");
+  const [file, setFile] = useState("");
   const navigate = useNavigate();
-  const userInfo = localStorage.getItem("userInfo")
-    const infoid = JSON.parse(userInfo)
-    console.log("landing")
-    console.log(infoid)
-    const userid = infoid.id
-    console.log(userid)
+  const userInfo = localStorage.getItem("userInfo");
+  const infoid = JSON.parse(userInfo);
 
-  const id = userid
-  console.log(id)
+  console.log("landing");
+  console.log(infoid);
+  const userid = infoid.id;
+  console.log(userid);
+
+  const id = userid;
+  console.log(id);
 
   useEffect(() => {
     getUserById();
   }, []);
-
+  console.log(userInfo);
   const updateUser = async (e) => {
     e.preventDefault();
+    const form = new FormData();
+    form.append("picture", file);
     try {
+      const response = await axios.put(
+        `http://localhost:8000/api/v1/profile/cloudinary/${id}`,
+        form,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+      setPhotoProfile(response.data.url);
+      console.log(photo_profile);
       await axios.put(`http://localhost:8000/api/v1/profile/update/${id}`, {
         name: name,
         photo_profile: photo_profile,
         city: city,
         address: address,
-        no_hp: no_hp
+        no_hp: no_hp,
       });
       navigate("/");
     } catch (error) {
@@ -44,10 +59,12 @@ export default function InfoProfil() {
   };
 
   const getUserById = async () => {
-    const response = await axios.get(`http://localhost:8000/api/v1/users/${id}`);
-    console.log(response)
+    const response = await axios.get(
+      `http://localhost:8000/api/v1/users/${id}`
+    );
+    console.log(response);
     setName(response.data.name);
-    setPhoto_profile(response.data.photo_profile);
+    // setPhoto_profile(response.data.photo_profile);
     setCity(response.data.city);
     setAddress(response.data.address);
     setNo_hp(response.data.no_hp);
@@ -55,59 +72,103 @@ export default function InfoProfil() {
 
   return (
     <div>
-      <nav className="navbar navbar-expand-lg bg-light d-inline-flex"
-        style={{ justifyContent: 'space-between', alignItems: 'center', width: '100%' }}>
-        <div className="d-inline-flex" style={{ padding: '10px', justifyContent: 'center', alignItems: 'center' }}>
-          <a className="navbar-brand" href='/'> <img src={Rectangle127} alt='' />
+      <nav
+        className="navbar navbar-expand-lg bg-light d-inline-flex"
+        style={{
+          justifyContent: "space-between",
+          alignItems: "center",
+          width: "100%",
+        }}
+      >
+        <div
+          className="d-inline-flex"
+          style={{
+            padding: "10px",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <a className="navbar-brand" href="/">
+            {" "}
+            <img src={Rectangle127} alt="" />
           </a>
         </div>
 
-        <div className="d-inline-flex" style={{ justifyContent: 'center', alignItems: 'center' }}>
-          <span className="navbar-brand mb-0 h1" style={{ fontWeight: '400px' }}>Lengkapi info Akun</span>
+        <div
+          className="d-inline-flex"
+          style={{ justifyContent: "center", alignItems: "center" }}
+        >
+          <span
+            className="navbar-brand mb-0 h1"
+            style={{ fontWeight: "400px" }}
+          >
+            Lengkapi info Akun
+          </span>
         </div>
 
-        <div className="d-inline-flex" style={{ justifyContent: 'center', alignItems: 'center' }}>
+        <div
+          className="d-inline-flex"
+          style={{ justifyContent: "center", alignItems: "center" }}
+        >
           <span className="navbar-brand mb-0 h1"></span>
         </div>
       </nav>
 
-
       <section>
         <form onSubmit={updateUser}>
-          <div className="container" style={{ padding: '30px', width: '70%' }}>
-            <a href="/"> <img src={arrow} alt="" /></a>
-            <label for="file-input" style={{ justifyContent: 'center', display: 'flex', alignItems: 'center' }}>
-              <img src={Group1} alt="" />
+          <div className="container" style={{ padding: "30px", width: "70%" }}>
+            <a href="/">
+              {" "}
+              <img src={arrow} alt="" />
+            </a>
+            <label
+              for="file-input"
+              style={{
+                justifyContent: "center",
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              {photo_profile ? (
+                <img src={photo_profile} alt="Uploaded Image URL" />
+              ) : (
+                <img src={Group1} alt=".." />
+              )}
             </label>
             <input
-              id='file-input'
+              id="file-input"
               type="file"
-              style={{ display: 'none' }}
+              style={{ display: "none" }}
               accept=".jpg,.jpeg,.png"
               value={photo_profile}
-              onChange={(e) => setPhoto_profile(e.target.value)}
-
+              onChange={(e) => setFile(e.target.files[0])}
             />
             <div className="mb-3">
-              <label for="nama" className="form-label">Nama<span style={{ color: 'red}' }}>*</span></label>
+              <label for="nama" className="form-label">
+                Nama<span style={{ color: "red}" }}>*</span>
+              </label>
               <input
                 type="text"
                 className="form-control"
-                id="nama" placeholder="Nama"
+                id="nama"
+                placeholder="Nama"
                 required
-                style={{ borderRadius: '16px' }}
+                style={{ borderRadius: "16px" }}
                 value={name}
                 onChange={(e) => setName(e.target.value)}
               />
             </div>
             <div className="mb-3">
-              <label for="kota" className="form-label">Kota<span style={{ color: 'red' }}>*</span></label>
+              <label for="kota" className="form-label">
+                Kota<span style={{ color: "red" }}>*</span>
+              </label>
               <input
                 type="text"
                 className="form-control"
-                id="kota" placeholder="Kota"
+                id="kota"
+                placeholder="Kota"
                 required
-                style={{ borderRadius: '16px' }}
+                style={{ borderRadius: "16px" }}
                 value={city}
                 onChange={(e) => setCity(e.target.value)}
               />
@@ -119,39 +180,52 @@ export default function InfoProfil() {
               </select> */}
             </div>
             <div className="mb-3">
-              <label for="alamat" className="form-label">Alamat<span style={{ color: 'red' }}>*</span></label>
+              <label for="alamat" className="form-label">
+                Alamat<span style={{ color: "red" }}>*</span>
+              </label>
               <input
                 type="text"
                 className="form-control"
                 id="alamat"
                 placeholder="contoh: Jalan Ikan Hiu 33"
                 required
-                style={{ borderRadius: '16px' }}
+                style={{ borderRadius: "16px" }}
                 value={address}
-                onChange={(e) => setAddress(e.target.value)} />
+                onChange={(e) => setAddress(e.target.value)}
+              />
             </div>
             <div className="mb-3">
-              <label for="nohp" className="form-label">No. Handphone<span style={{ color: 'red' }}>*</span></label>
+              <label for="nohp" className="form-label">
+                No. Handphone<span style={{ color: "red" }}>*</span>
+              </label>
               <input
                 type="number"
-                min='11'
+                min="11"
                 className="form-control"
                 id="nohp"
                 placeholder="contoh: 08123456789"
                 required
-                style={{ borderRadius: '16px' }}
+                style={{ borderRadius: "16px" }}
                 value={no_hp}
-                onChange={(e) => setNo_hp(e.target.value)} />
+                onChange={(e) => setNo_hp(e.target.value)}
+              />
             </div>
             <div className="mb-3">
-              <button type="submit" className="btn btn-primary" style={{ width: '100%', backgroundColor: '#7126B5', borderRadius: '16px' }}>
+              <button
+                type="submit"
+                className="btn btn-primary"
+                style={{
+                  width: "100%",
+                  backgroundColor: "#7126B5",
+                  borderRadius: "16px",
+                }}
+              >
                 Simpan
               </button>
             </div>
           </div>
         </form>
       </section>
-
     </div>
-  )
+  );
 }
